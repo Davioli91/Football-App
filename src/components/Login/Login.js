030123f9f7bcb5a550da88036559f6dba6e866e7
Dawid <you@example.com>
2020-04-29 21:15:59 +0200

    added modal to sign up and log opctions

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Modal, Input } from 'semantic-ui-react'
 
 const LoginBtn = () => (
   
       <div style={{ marginLeft: "50%" }}>
-      <Button primary>Sign up</Button>
-      <Button>Log-in</Button>
+      <Modal
+    trigger={<Button primary>Sign up</Button>}
+    header='You can here Sign up'
+    content='Call Benjamin regarding the reports.'
+    actions={['Cancel', { key: 'done', content: 'Sign up', positive: true }]}
+    
+  />
+  <Modal
+    trigger={<Button>Log-in</Button>}
+    header='You can here Log-in'
+    content='Call Benjamin regarding the reports.'
+    actions={['Cancel', { key: 'done', content: 'Log-in', positive: true }]}
+    
+  />
       </div>
     
 )
